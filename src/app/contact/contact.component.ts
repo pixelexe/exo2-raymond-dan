1183dea8deaf2a79b3d09a6e070ffcb9a1eb4988
raymond <rerezheng@gmail.com>
2024-12-10 17:57:19 +0100

    submit not complete

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  formContact = new FormGroup({
+    prenom: new FormControl('', Validators.required),
+    nom: new FormControl('', Validators.required),
+    age: new FormControl(''),
+    hasEmail: new FormControl(false),
+    email: new FormControl('',  Validators.email),
+    commentaire: new FormControl('', Validators.required),
 
+  });
+
+  addMailValidator() {
+    var emailControl = this.formContact.get('hasEmail');
+    if (emailControl?.value) {
+      this.formContact.get('email')?.addValidators(Validators.required);
+      
+    }
+    else {
+      this.formContact.get('email')?.removeValidators(Validators.required);
+      this.formContact.get('email')?.setValue('');
+
+    }
+    console.log(this.formContact.getRawValue());
+  }
+  
+  onSubmit() {
+    alert('Le formulaire est valide');
+  }
 }
